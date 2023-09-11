841878304054f5181ad5986c380540a3acf49a60
Al3xBrs <alexbrise.40@gmail.com>
2023-09-11 12:23:31 +0200

    Add tests for routes index & showSummary

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -1,4 +1,3 @@
-import json
 from ..server import loadClubs, loadCompetitions
 
 
---
+++ tests/test_routes.py
@@ -0,0 +1,46 @@
+from ..server import (
+    index,
+    showSummary,
+    book,
+    purchasePlaces,
+    points_display,
+    logout,
+    loadClubs,
+    loadCompetitions,
+    app,
+)
+
+import pytest
+from flask import Flask
+from flask_testing import TestCase
+
+
+class TestRoutes(TestCase):
+    def create_app(self):
+        app.config["TESTING"] = True
+        return app
+
+    def test_should_return_code_ok(self):
+        responses = [
+            self.client.get("/"),
+            self.client.get("/points_display"),
+        ]
+        for response in responses:
+            self.assert200(response)
+
+    def test_should_connect_correct_club(self):
+        response = self.client.post("/showSummary", data={"email": "test@test.fr"})
+        self.assert200(response)
+        self.assertIn("test@test.fr", response.get_data(as_text=True))
+
+    def test_should_return_correct_competitions(self):
+        response = self.client.post(
+            "/showSummary",
+            data={
+                "email": "test@test.fr",
+            },
+        )
+        self.assert200(response)
+        competitions = loadCompetitions()
+        for comp in competitions:
+            self.assertIn(f"{comp['name']}", response.get_data(as_text=True))
