10e9cb836a6e712108679f00522597a6d6e793f1
Al3xBrs <alexbrise.40@gmail.com>
2023-09-25 10:38:03 +0200

    Solved wrong mail making crash website

--- server.py
+++ server.py
@@ -61,8 +61,13 @@ def index():
 
 @app.route("/showSummary", methods=["POST"])
 def showSummary():
-    club = [club for club in clubs if club["email"] == request.form["email"]][0]
-    return render_template("welcome.html", club=club, competitions=competitions)
+    try:
+        club = [club for club in clubs if club["email"] == request.form["email"]][0]
+        return render_template("welcome.html", club=club, competitions=competitions)
+
+    except:
+        flash("This email DOES NOT exist. Try again.")
+        return render_template("index.html")
 
 
 @app.route("/book/<competition>/<club>")
